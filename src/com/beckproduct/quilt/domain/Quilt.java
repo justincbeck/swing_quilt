ff0a1951e60492cef1c0400b388d7b8ef49b22fa
jbeck <jbeck@074a344c-0dde-4714-8ea4-6c3656a3fbf0>
2009-03-17 04:03:51 +0000

    Fixed xdoclet stuff.  Switched from a list to a set.
    git-svn-id: svn+ssh://svn.beckproduct.com/svnroot/swingquilt/trunk@39 074a344c-0dde-4714-8ea4-6c3656a3fbf0

--- src/com/beckproduct/quilt/domain/Quilt.java
+++ src/com/beckproduct/quilt/domain/Quilt.java
@@ -19,11 +19,12 @@ public class Quilt extends JPanel
 {
     private String id;
 
-    public ArrayList tiles;
+    public LinkedHashSet tiles;
 
     public Quilt(LayoutManager layoutManager)
     {
         super(layoutManager);
+        this.tiles = new LinkedHashSet();
     }
 
     public Component add(Component component)
@@ -52,15 +53,18 @@ public class Quilt extends JPanel
     }
 
     /**
-     * @hibernate.list name="tiles" lazy="false" cascade="all"
-     * @hibernate.collection-one-to-many class="com.beckproduct.quilt.domain.QuiltTile"
+     * @hibernate.set name="tiles" lazy="false" cascade="all" order-by="time"
+     * @hibernate.collection-key column="quilt_id"
+     * @hibernate.collection-one-to-many class="com.beckproduct.quilt.domain.Quilt"
+     *
+     * @return Returns the tiles.
      */
-    public ArrayList getTiles()
+    public LinkedHashSet getTiles()
     {
         return tiles;
     }
 
-    public void setTiles(ArrayList tiles)
+    public void setTiles(LinkedHashSet tiles)
     {
         this.tiles = tiles;
     }

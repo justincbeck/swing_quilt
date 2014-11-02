11676772dc9d79e4f628b3ba0bb5a32a13ff1633
Justin C. Beck <justinbeck@mac.com>
2014-11-02 09:38:14 -0500

    switch file to maven from ant

---
+++ src/main/java/com/beckproduct/quilt/App.java
@@ -0,0 +1,13 @@
+package com.beckproduct.quilt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
